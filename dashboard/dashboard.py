2bef9a690f7330de5c9648ba37f21726db83c058
ririritsuu <mauritiusmanurungcreed@gmail.com>
2024-06-25 18:34:29 +0800

    sixth commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,7 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
 import logging 
 from babel.numbers import format_currency
 sns.set(style='dark')
